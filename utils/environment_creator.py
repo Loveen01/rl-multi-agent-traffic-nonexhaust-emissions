b7bf69b4227a835c77af48bcd20e582f1fff59ab
loveen011 <loveenomar05@gmail.com>
2024-02-23 20:30:21 +0000

    add utils dir

---
+++ utils/environment_creator.py
@@ -0,0 +1,51 @@
+import os 
+import pathlib
+
+import sumo_rl
+from sumo_rl.environment.env import env, parallel_env, SumoEnvironment
+
+import ray
+
+from ray import air, tune
+
+from ray.tune import register_env
+from ray.rllib.env import PettingZooEnv, ParallelPettingZooEnv
+from ray.rllib.algorithms import ppo 
+from ray.rllib.env.wrappers.multi_agent_env_compatibility import MultiAgentEnvCompatibility
+from ray.rllib.env.multi_agent_env import MultiAgentEnv
+
+from pettingzoo.utils.conversions import aec_to_parallel_wrapper
+from pettingzoo.utils import wrappers
+
+import sys
+sys.path.append('/Users/loveen/Desktop/Masters project/rl-multi-agent-traffic-nonexhaust-emissions')
+from environment.envs import RealMultiAgentSumoEnv
+from environment.observation import Grid2x2ObservationFunction, EntireObservationFunction
+from environment.reward_functions import combined_reward
+
+from datetime import datetime
+
+
+def par_env_2x2_creator(seed, eval, csv_path, tb_log_dir=None):
+        
+        env_folder = "data/2x2grid"
+
+        env_params = {"net_file": os.path.join(env_folder, "2x2.net.xml"),
+                "route_file": os.path.join(env_folder, "2x2.rou.xml"),
+                "reward_fn": combined_reward,
+                "observation_class": EntireObservationFunction, 
+                "out_csv_name": "outputs/2x2grid/ppo", 
+                "num_seconds": 100000000,
+                "add_per_agent_info": True,
+                "add_system_info": True,
+                "sumo_seed": seed,
+                "single_agent": False}
+        
+        marl_aec_pz_env = RealMultiAgentSumoEnv(**env_params, eval=eval, csv_path=csv_path) 
+
+        marl_aec_pz_env_asserted = wrappers.AssertOutOfBoundsWrapper(marl_aec_pz_env)
+        marl_aec_pz_env_order_enfor = wrappers.OrderEnforcingWrapper(marl_aec_pz_env_asserted)
+        
+        marl_par_env = aec_to_parallel_wrapper(marl_aec_pz_env_order_enfor)
+        
+        return marl_par_env
--- utils/utils.py
+++ utils/utils.py
@@ -12,7 +12,7 @@ def convert_arr_to_tensor(obs:np.ndarray):
 def get_agent_observation_as_tensor(all_agents_obs:dict, agent_id:str):
     '''Takes in entire observations in dict, returning observations for particular agent in tensor'''
     if agent_id not in all_agents_obs.keys():
-        raise ValueError(f"Agent {agent_id} not present in observation dict")
+        raise ValueError(f"Agent {agent_id} not present in observation dict {all_agents_obs}")
 
     agent_obs = all_agents_obs[agent_id]
     return convert_arr_to_tensor(agent_obs)
@@ -23,3 +23,39 @@ def generate_sampler(batch_size, minibatch_size ):
     # divide this list into batches of size minibatch_size [1, 3, ..], [54, 76, 2..]
     sampler = BatchSampler(subset_sampler, minibatch_size, True) 
     return sampler 
+
+# def config_to_text(config):
+#     text = f"Config Settings:\n\n"
+#     text += f"DEVICE: {config.device}\n"
+#     text += f"state_dim: {config.state_dim}\n"
+#     text += f"action_dim: {config.action_dim}\n"
+#     text += f"lr: {config.lr}\n"
+#     text += f"no_hidden_layers: {config.no_hidden_layers}\n"
+#     text += f"no_episodes: {config.no_episodes}\n"
+#     text += f"rollout_length: {config.rollout_length}\n"
+#     text += f"no_training_iterations: {config.no_training_iterations}\n"
+#     text += f"optimisation_epochs: {config.optimisation_epochs}\n"
+#     text += f"minibatch_size: {config.minibatch_size}\n"
+#     text += f"discount: {config.discount}\n"
+#     text += f"ppo_ratio_clip: {config.ppo_ratio_clip}\n"
+#     text += f"gae_lamda: {config.gae_lamda}\n"
+#     # text += f"logger_dir: {config.logger_dir}\n"
+#     text += f"play_only: {config.play_only}\n"
+#     text += f"saved_checkpoint: {config.saved_checkpoint}\n"
+#     return text
+
+def config_to_text(config):
+    text = f"Config Settings:\n\n"
+    for attr, value in vars(config).items():
+        text += f"{attr}: {value}\n"
+    return text
+
+def save_configs(config, config_txt_file_path):
+    text = f"Config Settings:\n\n"
+    for attr, value in vars(config).items():
+        text += f"{attr}: {value}\n\n"
+    
+    with open(config_txt_file_path, 'w') as text_file:
+        text_file.write(text)
+    
+    print(f"Config text file saved to {config_txt_file_path}")
